e247491c80a4cb0d2314ce6ec422bdb7fe7ed2a9
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-11-11 00:40:37 +0500

    edit

--- Hackathon project milestone 4/script.js
+++ Hackathon project milestone 4/script.js
@@ -5,14 +5,18 @@ var resumeDisplayElement = document.getElementById('resume-display');
 form.addEventListener('submit', function (event) {
     event.preventDefault(); //prevent page reload
     // Collect Input value
+    var objective = document.getElementById('objective').value;
     var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
+    var FaterName = document.getElementById('FaterName').value;
     var phone = document.getElementById('phone').value;
+    var cnic = document.getElementById('cnic').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
     var education = document.getElementById('education').value;
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value;
     // Generate the resume content dynamically
-    var resumeHTML = "\n<h2><b>Editable Resume</b></h2>\n<h3><b>Personal Information</b></h3>\n<p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n<p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n<p><b>phone:</b><span contenteditable=\"true\">").concat(phone, "</span></p>\n\n<h3><b>Education</b></h3>\n<p contenteditable=\"true\">").concat(education, "</p>\n\n<h3><b>Experience</b></h3>\n<p contenteditable=\"true\">").concat(experience, "</p>\n\n<h3><b>Skills</b></h3>\n<p contenteditable=\"true\">").concat(skills, "</p>\n");
+    var resumeHTML = "\n<h2><b>Resume</b></h2>\n<h3><b>Personal Information</b></h3>\n<p><b>Objective:</b><span contenteditable=\"true\">".concat(objective, "</p>\n<p><b>Name:</b><span contenteditable=\"true\">").concat(name, "</p>\n<p><b>FatherName:</b><span contenteditable=\"true\">").concat(FaterName, "</p>\n<p><b>Contact:</b><span contenteditable=\"true\">").concat(phone, "</p>\n<p><b>Cnic:</b><span contenteditable=\"true\">").concat(cnic, "</p>\n<p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</p>\n<p><b>Address:</b><span contenteditable=\"true\">").concat(address, "</p>\n\n<h3><b>Education</b></h3>\n<p><span contenteditable=\"true\">").concat(education, "</p>\n\n<h3><b>Experience</b></h3>\n<p><span contenteditable=\"true\">").concat(experience, "</p>\n\n<h3><b>Skills</b></h3>\n<p><span contenteditable=\"true\">").concat(skills, "</p>\n");
     // Display the generated resume
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
--- Hackathon project milestone 4/script.ts
+++ Hackathon project milestone 4/script.ts
@@ -6,29 +6,37 @@ form.addEventListener('submit', (event: Event) => {
     event.preventDefault(); //prevent page reload
 
     // Collect Input value
+    const objective = (document.getElementById('objective') as HTMLInputElement).value
     const name = (document.getElementById('name') as HTMLInputElement).value
-    const email = (document.getElementById('email') as HTMLInputElement).value
+    const FaterName = (document.getElementById('FaterName') as HTMLInputElement).value
     const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const cnic = (document.getElementById('cnic') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const address = (document.getElementById('address') as HTMLInputElement).value
     const education = (document.getElementById('education') as HTMLInputElement).value
     const experience = (document.getElementById('experience') as HTMLInputElement).value
     const skills = (document.getElementById('skills') as HTMLInputElement).value
 
     // Generate the resume content dynamically
     const resumeHTML = `
-<h2><b>Editable Resume</b></h2>
+<h2><b>Resume</b></h2>
 <h3><b>Personal Information</b></h3>
-<p><b>Name:</b><span contenteditable="true">${name}</span></p>
-<p><b>Email:</b><span contenteditable="true">${email}</span></p>
-<p><b>phone:</b><span contenteditable="true">${phone}</span></p>
+<p><b>Objective:</b><span contenteditable="true">${objective}</p>
+<p><b>Name:</b><span contenteditable="true">${name}</p>
+<p><b>FatherName:</b><span contenteditable="true">${FaterName}</p>
+<p><b>Contact:</b><span contenteditable="true">${phone}</p>
+<p><b>Cnic:</b><span contenteditable="true">${cnic}</p>
+<p><b>Email:</b><span contenteditable="true">${email}</p>
+<p><b>Address:</b><span contenteditable="true">${address}</p>
 
 <h3><b>Education</b></h3>
-<p contenteditable="true">${education}</p>
+<p><span contenteditable="true">${education}</p>
 
 <h3><b>Experience</b></h3>
-<p contenteditable="true">${experience}</p>
+<p><span contenteditable="true">${experience}</p>
 
 <h3><b>Skills</b></h3>
-<p contenteditable="true">${skills}</p>
+<p><span contenteditable="true">${skills}</p>
 `;
 // Display the generated resume
 if(resumeDisplayElement){
@@ -37,3 +45,4 @@ if(resumeDisplayElement){
     console.error('The resume display element is missing.')
 }
 })
+
